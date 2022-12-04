1b58a51b8b66dc4a75d03dd3b32335c782dbdb97
Arewee <90697787+Arewee@users.noreply.github.com>
2022-12-04 12:47:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// starta denna med: nodemon app
+// starta också MongoDV server =>
+// https://cloud.mongodb.com/v2/6388c2da8e4d865c34143fef#clusters/connect?clusterId=Cluster0
+// kan behöva lägga in ny "connection string" nedan med un/pw.
+
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI =
+  "mongodb+srv://rillepilleGOZ:JryD9AcGaM2msiTp@cluster0.etoqbri.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// register view engine
+app.set("view engine", "ejs");
+
+// middleware & static files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+// blog routes
+app.use("/blogs", blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
